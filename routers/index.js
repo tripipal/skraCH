7ce041dd861098ea75d8a2c3f86b8f4ccd7870e6
udarakr <udarakr@gmail.com>
2014-05-08 12:52:18 +0530

    adding tmp authentication logic

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,21 @@
 //Generate routing requests according to the VERB
 var router = require('./controller');
 
+//passport configuration
+//TODO this should moved to the config dir
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: '831290403571532',
+    clientSecret: '2c0e06153fff18bb910f00c12dcf7f5b',
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+  }
+));
+
 module.exports = function(app) {
     
     // handle API calls
@@ -10,7 +25,13 @@ module.exports = function(app) {
     app.put('/api/:api/:id', router.put);
     app.delete('/api/:api/:id', router.delete);
     // load application
-    app.get('*', function(req, res) {
+    /*app.get('*', function(req, res) {
 		res.sendfile('./public/index.html'); // angular will handle this
-	});
+	});*/
+    
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', { scope: 'read_stream' })
+    );
+    app.get('/auth/facebook/callback', 
+        passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
 }
